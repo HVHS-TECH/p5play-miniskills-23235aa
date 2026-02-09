7b19b7d9160406be75f07afbdfba9d9a8916aca6
23235aa <23235aa@hvhs.school.nz>
2026-02-09 13:28:20 +1300

    collision

--- t04_collision.js
+++ t04_collision.js
@@ -9,14 +9,20 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-
+cnv= new Canvas(1000,1000);
+Rectangle = new Sprite(200,200,200,200,'d');
+Rectangle.colour = '#0000FF';
+Rectangle.rotationSpeed = 2;
+Rectangle.vel.x=2;
+world.gravity.y=10;
+platform_1 = new Sprite(500,300,300,400,'k');
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('white')
 }
 
 /*******************************************************/
