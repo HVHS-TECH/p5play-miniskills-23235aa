2f5db53d7759a0bbf66e9348deb75983a1253707
23235aa <23235aa@hvhs.school.nz>
2026-02-19 11:00:48 +1300

    done

--- t22_keyboard.js
+++ t22_keyboard.js
@@ -9,14 +9,32 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-
+cnv= new Canvas(1000,1000);
+Rectangle = new Sprite(200,200,200,200,'d');
+Rectangle.colour = '#0000FF';
+Rectangle.rotationSpeed = 2;
+Rectangle.vel.x=2;
+world.gravity.y=10;
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('white')
+	Rectangle.moveTowards(mouseX, mouseY, 0.5);
+	if (kb.pressing('left')){
+		Rectangle.vel.x=-2
+	}
+	else if (kb.pressing('right')){
+		Rectangle.velocity.x=2
+	};
+	if (kb.released('left')){
+		Rectangle.vel.x=0
+	}
+	else if(kb.released('right')){
+		Rectangle.vel.x=0
+	};
 }
 
 /*******************************************************/
---
+++ t23_text.js
@@ -0,0 +1,8 @@
+function setup() {
+	console.log("setup: ");
+}
+function draw(){
+    text("Hello World", 30, 50)
+    var name = "Big Boy";
+    text("Hello "+name, 30, 100);
+}
